e774737c53e566b7b40715c74c1c55473615f76e
Phil Barker <pbarker@impinj.com>
2017-01-10 18:13:50 +0000

    Additional test from prior serialized form.

--- src/test/java/com.impinj.itemsense.client/coordinator/recipe/RecipeTest.java
+++ src/test/java/com.impinj.itemsense.client/coordinator/recipe/RecipeTest.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.time.Duration;
 
 
@@ -34,6 +35,22 @@ public class RecipeTest {
     Assert.assertEquals("RECIPE", recipeDeserialized.getName());
     Assert.assertEquals(RecipeType.LOCATION, recipeDeserialized.getType());
     Assert.assertEquals(tagExpiry, recipeDeserialized.getTagExpiryDuration());
+  }
+
+  @Test
+  public void test2016R6Compatability2017R1() throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule());
+    mapper.configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false);
+    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+
+    String string = "{\"name\":\"RECIPE\",\"type\":\"LOCATION\",\"tagExpiryDuration\":\"PT10S\",\"readerConfigurations\":{},\"minimumMovementInMeters\":1.0,\"computeWindow\":10,\"reportingInterval\":5}";
+
+    Recipe recipeDeserialized = mapper.readValue(string, Recipe.class);
+    Assert.assertEquals("RECIPE", recipeDeserialized.getName());
+    Assert.assertEquals(RecipeType.LOCATION, recipeDeserialized.getType());
+    Assert.assertEquals(Duration.ofSeconds(10), recipeDeserialized.getTagExpiryDuration());
   }
 
   @Test
